cdae7a5170af4a2b8eddc205e06de253314fa115
Brumassio <dibrunspeed@gmail.com>
2022-01-22 21:58:27 -0300

    Comparando value types e reference types

---
+++ Estagio_GFT/compar_value_ref/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace compar_value_ref // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        class Numero
+        {
+            public int N { get; set; }
+            public Numero(int n)
+            {
+                N = n;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Numero i = new Numero(2);
+            Numero j = new Numero(2);
+            //int a = 2;
+            //int b = 2;
+            if(i.N == j.N)
+            {
+                WriteLine("a e b são iguais");
+            }
+            else
+            {
+                WriteLine("a e b são diferentes!");
+            }
+
+
+
+        }
+    }
+}
