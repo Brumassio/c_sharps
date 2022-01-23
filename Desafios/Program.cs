82cd8650d06e90cb7a07591c01c54fd271d70274
Brumassio <dibrunspeed@gmail.com>
2022-01-22 21:54:29 -0300

    Abstraindo um jogo de RPG usando OO com C#

---
+++ Desafios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Desafios.src;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arus hero = new Arus("Joseph",12,"Knight");
+        }
+    }
+   
+}
