4abfd43edb5b7463a768acf366cd50a620c5271a
Brumassio <dibrunspeed@gmail.com>
2022-01-27 00:01:40 -0300

    Monitorando tudo e todos

---
+++ Estagio_GFT/Arquivos_e_Streams/FileSystemWatcher/Program.cs
@@ -0,0 +1,28 @@
+﻿//Detectar alterações nos arquivos !!!!!
+var path = @"C:\Users\Diogo Brumassio\Desktop\Codigos_em_C#\Estagio_GFT\Arquivos\Directory_And_DirectoryInfo\globo";
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Deleted += OnDeleted;
+fsw.Renamed += OnRenamed;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine($"Monitorando eventos na pasta {path}");
+Console.WriteLine("Pressione [enter] para finalizar");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi criado o arquivo {e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Foi excluido o arquivo {e.Name}");
+}
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"O arquivo {e.OldName} foi renomeado para {e.Name}");
+}
