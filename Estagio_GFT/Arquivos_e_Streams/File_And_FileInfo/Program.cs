bff602d0c5b9fc7416e021b9e21f2e07127aaf2c
Brumassio <dibrunspeed@gmail.com>
2022-01-27 00:01:09 -0300

    File_And_FileInfo  :D

---
+++ Estagio_GFT/Arquivos_e_Streams/File_And_FileInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+WriteLine("Digite o nome do arquivo");
+var nome = ReadLine();
+
+foreach(var @char in Path.GetInvalidFileNameChars())
+{
+    nome = nome.Replace(@char, '-');
+}
+
+var path = Path.Combine(Environment.CurrentDirectory,$"{nome}.txt");
+
+CriarArquivo(path);
+
+WriteLine("Digite enter para finalizar ...");
+ReadLine();
+
+static  void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Está a linha 1 do arquivo");
+        sw.WriteLine("Está a linha 2 do arquivo");
+        sw.WriteLine("Está a linha 3 do arquivo");
+    }
+    catch 
+    {
+        
+        WriteLine("Erro nome inváilido");
+    }
+
+}
