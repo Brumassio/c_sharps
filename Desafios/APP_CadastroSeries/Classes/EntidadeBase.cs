03331bffdbb174421ca5e2ee74a1c5681cefdc90
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:12:20 -0300

    Classes utilizadads

---
+++ Desafios/APP_CadastroSeries/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace APP_CadastroSeries.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Desafios/APP_CadastroSeries/Classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using APP_CadastroSeries.Interfaces;
+
+namespace APP_CadastroSeries.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Exclui();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
