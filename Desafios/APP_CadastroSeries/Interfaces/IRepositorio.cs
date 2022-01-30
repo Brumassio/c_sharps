9cc896c248e26363d54fc767e1f35afcc8bb334f
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:13:11 -0300

    Interfaces

---
+++ Desafios/APP_CadastroSeries/Interfaces/IRepositorio.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+namespace APP_CadastroSeries.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
