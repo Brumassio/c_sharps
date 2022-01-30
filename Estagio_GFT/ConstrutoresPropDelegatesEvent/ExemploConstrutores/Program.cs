04a5ec1ffbaf43af807ec54034f262d9d3bb566b
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:01:22 -0300

    Program git add Program.cs !

--- Estagio_GFT/ConstrutoresPropDelegatesEvent/ExemploConstrutores/Program.cs
+++ Estagio_GFT/ConstrutoresPropDelegatesEvent/ExemploConstrutores/Program.cs
@@ -5,6 +5,7 @@ namespace ExemploConstrutores // Note: actual namespace depends on the project n
 {
     internal class Program
     {
+        public delegate void Operacao(int x, int y);
         static void demo1()
         {
             Log log = Log.GetInstance();
@@ -21,10 +22,38 @@ namespace ExemploConstrutores // Note: actual namespace depends on the project n
             Pessoa p1 = new Pessoa("Diogo","Brumassio");
             p1.Apresentar();
         }
-        static void Main(string[] args)
+
+        static void demo3()
         {
             Aluno p1 = new Aluno("Joseph","Robert","PAA");
             p1.Apresentar();
+        }
+
+        static void demo4()
+        {
+            Data d1 = new Data();
+            //d1.SetMes(333);
+            
+            d1.Mes = 20;
+            d1.ApresentarMes();
+        }
+        static void demo5()
+        {
+            const double pi = 3.14;
+            System.Console.WriteLine(pi);
+        }
+        static void demo6()
+        {
+            //Operacao op = Calculadora.Somar;
+            //op(12,1);
+            Operacao op = new Operacao(Calculadora.Somar);
+            op += Calculadora.Subtrair;
+            op.Invoke(12,1);
+        }
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica(10,20);
+            m.Somar();
         }
     }
 }
